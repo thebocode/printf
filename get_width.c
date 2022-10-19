ad4c4dc67d266c89fcd89ed4e09bf950ab0b726c
Bosun <bosunadebola@gmail.com>
2022-10-19 05:32:47 -0700

    updated printf codes

--- get_width.c
+++ get_width.c
@@ -1,13 +1,13 @@
-#include "main.h"
+include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: width.
+ * get_width - calculates the width for printing
+ * @format: formatted string in which to print the arguments
+ * @i: list of arguments to be printed
+ * @list: list of arguments
+ * Return: width
  */
+
 int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
